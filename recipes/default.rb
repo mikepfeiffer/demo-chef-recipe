7105fac6c6b3fdf7b5e7169d1208884973d80924
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2016-04-11 16:15:35 -0700

    added default recipe

---
+++ recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# All rights reserved - Do Not Redistribute
+#
+
+#Run apt-get update
+
+execute "apt-get update" do
+	command "apt-get update"
+end
+
+#Install apache
+
+package "apache2" do
+	action :install
+end
+
+#Start apache service and enable service
+
+service "apache2" do
+	action [:start, :enable]
+end
+
+cookbook_file "/var/www/html/index.html" do
+	source "index.html"
+	mode "0644"
+end
