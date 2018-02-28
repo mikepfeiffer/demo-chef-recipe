1731aa811b500df3516f9e87f8000f1da4f2f910
Gary Stafford <gary.stafford@accenture.com>
2018-02-28 13:16:11 -0500

    Fix index.html path

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@ service "apache2" do
     action [:start, :enable]
 end
 
-cookbook_file "/var/www/index.html" do
+cookbook_file "/var/www/html/index.html" do
     source "index.html"
     mode "0644"
 end
